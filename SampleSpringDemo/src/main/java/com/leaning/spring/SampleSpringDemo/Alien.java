3a0cd59e70b24e1481ef65bd24ea540e8c70703b
DhivyaViswanathan <dhivyaviswanathantce@gmail.com>
2018-12-22 15:04:10 +0530

    commit alien

--- SampleSpringDemo/src/main/java/com/leaning/spring/SampleSpringDemo/Alien.java
+++ SampleSpringDemo/src/main/java/com/leaning/spring/SampleSpringDemo/Alien.java
@@ -6,6 +6,24 @@ public class Alien {
 	private int aid;
 	private String aname;
 	private int marks;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
 	
 	
 }
